9140a29636ac7693c8a6025b85340dbb5abb4ff7
Flavian Dutoit <flavian.dutoit@viacesi.fr>
2019-12-17 10:23:50 +0100

    Initialisation du projet
    Initialisation de l'architecture du projet.
    Fichiers PHP/CSS/JS

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TP Series</title>
+        <link rel=¨stylesheet¨ type=¨text/css¨ href=¨CSS/style.css¨>
+    </head>
+
+    <body>
+
+    <script src="JS/script.js"></script>
+    <script src="JS/jquery-3.4.1.min.js"></script>
+    </body>
+</html>
